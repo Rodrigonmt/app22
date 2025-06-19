970aef05a1a5aea975e9ee6946440f1363351af8
Rodrigonmt <rodrigo372.nmt@gmail.com>
2025-06-19 01:01:26 -0300

    ConcluidoBTNCancelar

---
+++ app22/Classes/StatusToEnabledConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace app22.Classes
+{
+    public class StatusToEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString().Equals("Pendente", StringComparison.OrdinalIgnoreCase) == true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
