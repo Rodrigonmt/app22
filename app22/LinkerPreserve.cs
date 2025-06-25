a54f020e5d2eb89b4f00ed5e58197a246857a209
Rodrigonmt <rodrigo372.nmt@gmail.com>
2025-06-24 23:36:30 -0300

    ok

---
+++ app22/LinkerPreserve.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp.Views.Maui.Controls;
+//using Android.Runtime;
+
+namespace app22
+{
+    // Informa ao linker para manter essa classe e seus membros
+    [Preserve(AllMembers = true)]
+    public class LinkerPreserve
+    {
+        public void Include()
+        {
+            // Cria instância do SKCanvasView para evitar remoção pelo linker
+            var view = new SKCanvasView();
+            view.IgnorePixelScaling = false;
+
+            // Aqui você pode adicionar mais tipos que estão sendo removidos indevidamente
+        }
+    }
+}
---
+++ app22/PreserveAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app22
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+    }
+}
